99fed5ab944823fef84339d6c2d5b441fe4df83f
Will Munn <willmunn@hotmail.com>
2015-10-16 08:32:41 +0100

    Pull property for out to seperate file

--- scripts/properites/properties.js
+++ scripts/properites/properties.js
@@ -1,52 +1,9 @@
-const save = require('./save');
 const createRepo = require('../model');
 const repo = createRepo(localStorage);
 const React = require('react');
 const Modal = require('react-bootstrap/lib/Modal');
 const Button = require('react-bootstrap/lib/Button');
-
-const PropertyForm = React.createClass({
-
-    onSubmit(e) {
-        e.preventDefault();
-        save(e);
-        this.props.onPropertiesChanged();
-    },
-
-    render() {
-        return <form className="form-inline" action="#" id="add-property" onSubmit={this.onSubmit}>
-            <div className="form-group">
-                <label className="sr-only" htmlFor="property-name">Property Name</label>
-                <input className="form-control" type="text"
-                    name="property-name" id="property-name"
-                    placeholder="Property Name"
-                />
-            </div>
-            <div className="form-group">
-                    <label className="sr-only" htmlFor="property-value">Value</label>
-                    <input className="form-control" type="number"
-                        name="property-value" id="property-value"
-                        placeholder="Value"
-                    />
-            </div>
-            <div className="form-group">
-                <div className="input-group">
-                        <label className="sr-only" htmlFor="property-projected-increase">Yearly increase (%)</label>
-                        <input className="form-control" type="number"
-                            name="property-projected-increase"
-                            id="property-projected-increase"
-                            placeholder="Projected Increase"
-                        />
-                        <div className="input-group-addon">%</div>
-                </div>
-            </div>
-            <button className="btn btn-default" type="submit">
-                <span className="glyphicon glyphicon-plus" aria-hidden="true">Add</span>
-            </button>
-        </form>;
-    }
-
-});
+const PropertyForm = require('./property-form');
 
 module.exports = React.createClass({
     getInitialState() {
---
+++ scripts/properites/property-form.js
@@ -0,0 +1,45 @@
+const React = require('react');
+const save = require('./save');
+const PropertyForm = React.createClass({
+
+    onSubmit(e) {
+        e.preventDefault();
+        save(e);
+        this.props.onPropertiesChanged();
+    },
+
+    render() {
+        return <form className="form-inline" action="#" id="add-property" onSubmit={this.onSubmit}>
+            <div className="form-group">
+                <label className="sr-only" htmlFor="property-name">Property Name</label>
+                <input className="form-control" type="text"
+                    name="property-name" id="property-name"
+                    placeholder="Property Name"
+                />
+            </div>
+            <div className="form-group">
+                    <label className="sr-only" htmlFor="property-value">Value</label>
+                    <input className="form-control" type="number"
+                        name="property-value" id="property-value"
+                        placeholder="Value"
+                    />
+            </div>
+            <div className="form-group">
+                <div className="input-group">
+                        <label className="sr-only" htmlFor="property-projected-increase">Yearly increase (%)</label>
+                        <input className="form-control" type="number"
+                            name="property-projected-increase"
+                            id="property-projected-increase"
+                            placeholder="Projected Increase"
+                        />
+                        <div className="input-group-addon">%</div>
+                </div>
+            </div>
+            <button className="btn btn-default" type="submit">
+                <span className="glyphicon glyphicon-plus" aria-hidden="true">Add</span>
+            </button>
+        </form>;
+    }
+
+});
+module.exports = PropertyForm;
