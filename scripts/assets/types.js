7618acb231159fe84385618d9519f1afc0465878
Will Munn <willmunn@hotmail.com>
2015-11-28 09:14:17 +0000

    Ensure types are sorted by liquidity by default

--- scripts/assets/types.js
+++ scripts/assets/types.js
@@ -1,18 +1,18 @@
 module.exports = {
-    PROPERTY: {
-        name: 'Property',
-        liquidity: 1
-    },
     CASH: {
         name: 'Cash',
         liquidity: 4
+    },
+    ASSURANCE: {
+        name: 'Assurance',
+        liquidity: 3
     },
     SECURITIES: {
         name: 'Securities',
         liquidity: 2
     },
-    ASSURANCE: {
-        name: 'Assurance',
-        liquidity: 3
+    PROPERTY: {
+        name: 'Property',
+        liquidity: 1
     }
 };
